4213cb0a5a66dcb9fb96e4a309137c8c8025a6da
qiubin <384341396@qq.com>
2021-10-31 11:27:48 +0800

    更新文件

--- src/Day_001/Test_same.py
+++ src/Day_001/Test_same.py
@@ -4,10 +4,11 @@
 # @Author  : qiubin
 # @File    : Test_same.py
 # @Software: PyCharm
-from functools import reduce
-from PIL import Image
 import math
 import operator
+from functools import reduce
+
+from PIL import Image
 
 
 def compare(pic1, pic2):
@@ -16,16 +17,18 @@ def compare(pic1, pic2):
     :param pic2: 图片2路径
     :return: 返回对比的结果
     """
-    image1 = Image.open(pic1)
-    image2 = Image.open(pic2)
-
-    histogram1 = image1.histogram()
-    histogram2 = image2.histogram()
-
-    differ = math.sqrt(reduce(operator.add, list(map(lambda a, b: (a-b)**2, histogram1, histogram2)))/len(histogram1))
-
-    print(differ)
-    return differ
+    try:
+        image1 = Image.open(pic1)
+        image2 = Image.open(pic2)
+    except FileNotFoundError as e:
+        print(e)
+    else:
+        histogram1 = image1.histogram()
+        histogram2 = image2.histogram()
+        differ = math.sqrt(
+            reduce(operator.add, list(map(lambda a, b: (a - b) ** 2, histogram1, histogram2))) / len(histogram1))
+        print(differ)
+        return differ
 
 
 compare(r'D:\1_1.png', r'D:\1_3.png')
