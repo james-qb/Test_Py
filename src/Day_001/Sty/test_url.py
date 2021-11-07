47ee38da6b3361ec83408d8667333cd8515cec1f
qiubin <384341396@qq.com>
2021-11-07 17:13:30 +0800

    更新文件

--- src/Day_001/Sty/test_url.py
+++ src/Day_001/Sty/test_url.py
@@ -3,3 +3,19 @@ import urllib.parse
 data = "中国"
 quo_data = urllib.parse.quote(data)
 print(quo_data)
+
+a_list = [1, 2], [2, 3], [3, 4], [4, 5]
+print(*a_list)
+print(list(zip(*a_list)))
+
+a = [1, 2, 3, 4, 5, 6]
+# print(list(zip(*a)))
+for i in range(10):
+    for j in range(10):
+        if i + j > 5:
+            print(i, j)
+            break
+
+for i in range(5):
+    b = b + 1
+    print(b)
